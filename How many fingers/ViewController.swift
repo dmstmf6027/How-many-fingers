d597bd2e30772fba75e32e919d423adc3ea02e94
201631053 <hubot@githhub@com>
2018-03-26 15:50:51 +0900

    add

--- How many fingers/ViewController.swift
+++ How many fingers/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        input.delegate = self
+        input.clearButtonMode = UITextFieldViewMode.always
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func guess(_ sender: Any) {
+        // 랜덤 숫자 생성
+       let number = arc4random() % 6
+       print(number)
+       let numberString = String(number)
+        
+        if input.text == numberString {
+            resultLabel.text = "빙고 !!!"
+        } else {
+            resultLabel.text = "실패 ! 다시 해보세요 !!! \n숨겨진 숫자는 현재 \(number)입니다 !"
+        }
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        input.backgroundColor = UIColor.green
+    }
+        
 }
+
+    
 
